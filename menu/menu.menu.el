60e696fcae210c3553977ba44b07022db111b6c4
gwx <grayswandir76@gmail.com>
2013-03-10 09:07:29 -0400

    Minor menu change.

--- menu/menu.menu.el
+++ menu/menu.menu.el
@@ -4,7 +4,7 @@
   | open with a call to `elxiki-command', bound to C-<return>
   | and M-<return> by default. Menus can expand to sub-menu
   | items, as well as run arbitrary emacs lisp code.
-|
+  |
 + Defining Menus
   | To define a menu, type the name of one and then call `elxiki-menu-edit',
   | bound to C-c [ by default. This will find the default file for that menu.
@@ -26,7 +26,7 @@
   | Menus are explicitly declared with an @ prefix. This can
   | usually be skipped, unless you want to link from one menu
   | to another.
-|
+  |
 > Here are all the defined menus:
 @ all/
 ")
